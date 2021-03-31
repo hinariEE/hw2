d69c141e92dcc84f1869fa40de3530507b8901e2
hinariEE <esugarmaple@gmail.com>
2021-03-31 04:14:17 -0700

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ AnalogOut Aout(D7);
 AnalogIn Ain(A0);
 uLCD_4DGL uLCD(D1, D0, D2);
 
-int idx;
-int smaple = 512;
-float ADCdata[512];
+int idx = 0;
+int sample = 1024;
+float ADCdata[1024];
 
 void cursor_update(int pos, int select)
 {
@@ -23,15 +23,18 @@ void cursor_update(int pos, int select)
     uLCD.printf(" ");
     uLCD.locate(0, 1);
     uLCD.printf(" ");
+    uLCD.locate(0, 2);
+    uLCD.printf(" ");
+    uLCD.locate(0, 3);
+    uLCD.printf(" ");
     uLCD.locate(0, pos);
     uLCD.printf("v");
 }
 
 void sample_output()
 {
-    for(int i = 0; i < smaple; i++){
+    for(int i = 0; i < sample; i++){
         printf("%f\n", ADCdata[i]);
-        ThisThread::sleep_for(5ms);
     }
 }
 
@@ -44,9 +47,13 @@ int main()
     uLCD.color(GREEN);
     uLCD.reset();
     uLCD.locate(2, 0);
-    uLCD.printf("100 Hz");
+    uLCD.printf(" 50 Hz");
     uLCD.locate(2, 1);
+    uLCD.printf("100 Hz");
+    uLCD.locate(2, 2);
     uLCD.printf("200 Hz");
+    uLCD.locate(2, 3);
+    uLCD.printf("300 Hz");
     
     int cursor = 0;
     uLCD.locate(0, 0);
@@ -56,10 +63,14 @@ int main()
     int sample_on = 0;
     float i = 0.0f;
     float step;
-    float rise_list[] = { 10.0f / 9.0f * 0.00001f * 100.0f * 2.85f,  // 100 Hz
-                          10.0f / 9.0f * 0.00001f * 200.0f * 2.84f}; // 200 Hz
-    float fall_list[] = {-10.0f / 1.0f * 0.00001f * 100.0f * 2.88f,
-                         -10.0f / 1.0f * 0.00001f * 200.0f * 2.88f};
+    float rise_list[] = { 10.0f / 9.0f * 0.0001f *  50.0f * 0.285f,  // 50 Hz
+                          10.0f / 9.0f * 0.0001f * 100.0f * 0.285f,  // 100 Hz
+                          10.0f / 9.0f * 0.0001f * 200.0f * 0.29f,   // 200 Hz
+                          10.0f / 9.0f * 0.0001f * 300.0f * 0.29f};  // 300 Hz
+    float fall_list[] = {-10.0f / 1.0f * 0.0001f *  50.0f * 0.285f,
+                         -10.0f / 1.0f * 0.0001f * 100.0f * 0.285f,
+                         -10.0f / 1.0f * 0.0001f * 200.0f * 0.29f,
+                         -10.0f / 1.0f * 0.0001f * 300.0f * 0.29f};
     float rise = rise_list[0];
     float fall = fall_list[0];
     while(1){
@@ -75,7 +86,7 @@ int main()
         if(down){
             if(btn_prev == 0){
                 btn_prev = 1;
-                if(cursor != 1){
+                if(cursor != 3){
                     cursor++;
                     cursor_update(cursor, 0);
                 }
@@ -87,6 +98,7 @@ int main()
                 rise = rise_list[cursor];
                 fall = fall_list[cursor];
                 cursor_update(cursor, 1);
+                i = 0.0f;
                 sample_on = 1;
                 idx = 0;
             }
@@ -106,16 +118,15 @@ int main()
         i += step;
         Aout = i;
         ADCdata[idx] = Ain;
-        
+            
         if(sample_on){
             idx++;
-            if(idx == smaple){
+            if(idx == sample){
                 idx = 0;
                 sample_on = 0;
                 sample_output();
-                ThisThread::sleep_for(1ms/10);
             }
         }
-        
+        ThisThread::sleep_for(1ms/10);
     }
 }
